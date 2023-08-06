20aced1f8c6d969741b7aef10ce486198a05fb2b
Vyndrean <sebastian_morenoi@hotmail.com>
2023-08-06 01:37:00 -0400

    deleted puppeter and updated dockerfile

--- backend/routes/pdfRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const pdfController = require('../controllers/pdfController')
-
-router.post('/generate-pdf', pdfController.generatePDF)
-
-module.exports = router
